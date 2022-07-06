8d214d4799c0e40b4ecf25d647520c377178bef9
lwyKunKun <you@example.com>
2022-07-06 23:42:19 +0800

    首页完成1%

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
+//elmentui css文件
+import "element-plus/theme-chalk/display.css";
 
 // router
 import router from "./router/index";
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,15 +5,33 @@
  * index.vue
 -->
 <template>
-  <div class="home-container">1111</div>
+  <div class="home-container">
+    <!--适配电脑-->
+    <header
+      :md="24"
+      :xl="24"
+      :lg="24"
+      class="hidden-sm-and-down computer-header"
+    >
+      <div class="logo">
+        <img src="../../assets/img/logo/trypto_logo_blue.png" alt="" />
+      </div>
+      <nav></nav>
+    </header>
+  </div>
 </template>
 
 <script setup>
-import { reactive, ref, defineProps, defineEmits } from 'vue'
-defineProps({})
+import { reactive, ref } from 'vue'
 </script>
 <style lang="scss" scoped>
 .home-container {
-  color: pink;
+  width: 100%;
+  height: 100vh;
+  background: url('../../assets/img/bg/banner_bg.jpeg') no-repeat;
+  color: #fff;
+  .computer-header {
+    width: 100%;
+  }
 }
 </style>
